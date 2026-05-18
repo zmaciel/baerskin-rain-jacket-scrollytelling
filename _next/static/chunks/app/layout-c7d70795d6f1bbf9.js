e9ee03c3513dfdd5e23f4a02c031aee6550f06a6
zmaciel <46382529+zmaciel@users.noreply.github.com>
2026-05-18 15:05:46 +0200

    Deploy static site

---
+++ _next/static/chunks/app/layout-c7d70795d6f1bbf9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1743:()=>{},8623:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1743,23))}},e=>{e.O(0,[334,441,794,358],()=>e(e.s=8623)),_N_E=e.O()}]);
